22d0bfbd7d44a99025ee9e0c6bd2b6e7073741b2
Mahdi <mahdisafari20002013@gmail.com>
2018-09-22 05:08:59 -0700

    edit home page

---
+++ js/news-event.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+    $(".post-module").hover(function() {
+        $(this)
+            .find(".description")
+            .stop()
+            .animate(
+                {
+                    height: "toggle",
+                    opacity: "toggle"
+                },
+                300
+            );
+    });
+});
